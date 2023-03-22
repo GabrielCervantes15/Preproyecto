8266f7f7bcc1bb7e5b58e28ef4e2cc18ac2c3a8b
GabrielCervantes15 <125184081+GabrielCervantes15@users.noreply.github.com>
2023-03-21 17:12:10 -1200

    Add files via upload

---
+++ Preproyecto/src/main/java/Persistencias/VideojuegoDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencias;
+
+import Entidades.Videojuegos;
+import java.awt.List;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class VideojuegoDAO {
+
+}
---
+++ Preproyecto/src/main/java/com/mycompany/preproyecto/Preproyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.preproyecto;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Preproyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
